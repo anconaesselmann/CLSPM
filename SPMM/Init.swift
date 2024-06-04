80669f1ba705d15724cc67d2fd265a641b3830d2
Axel Ancona Esselmann <axel@anconaesselmann.com>
2024-06-04 16:29:06 -0700

    Initialize with cached dependencies

--- SPMM/Init.swift
+++ SPMM/Init.swift
@@ -7,6 +7,10 @@ import XProjParser
 
 struct Init: ParsableCommand {
 
+    enum Error: Swift.Error {
+        case invalidCachedDependencies([String])
+    }
+
     public static let configuration = CommandConfiguration(
         abstract: "Initialize an spmfile"
     )
@@ -17,17 +21,79 @@ struct Init: ParsableCommand {
     )
     private var spmfile: String?
 
+    @Option(
+        name: .shortAndLong,
+        help: "Dependency names for dependencies cached with `dependency-cache`"
+    )
+    var cached: [String] = []
+
+    @Option(
+        name: .shortAndLong,
+        help: "Only perform command on a given target"
+    )
+    var target: String?
+
     @Flag(
         name: .shortAndLong,
         help: "Show extra logging"
     )
     private var verbose: Bool = false
 
+    @Flag(
+        name: .shortAndLong,
+        help: "Force reinitialization of all targets"
+    )
+    private var force: Bool = false
+
     func run() throws {
         let project = try Project()
         let root = try project.root()
         let targets = try project.targets(in: root)
-        let targetDependencies = try project.dependencies(in: root, verbose: verbose)
+        var targetDependencies = try project.dependencies(in: root, verbose: verbose)
+        let cached = Set(cached)
+        if !cached.isEmpty {
+            let configManager = ConfigManager()
+            let cachedDependencies = try configManager
+                .dependenciesFile().dependencies
+                .filter {
+                    cached.contains($0.name)
+                }
+            let used = Set(cachedDependencies.map { $0.name })
+            let notUsed = cached.subtracting(used)
+            guard notUsed.isEmpty else {
+                throw Error.invalidCachedDependencies(notUsed.sorted())
+            }
+            for var (targetName, dependencies) in targetDependencies {
+                if let target = target {
+                    if targetName != target {
+                        vPrint("Ignoring target \(targetName)", verbose)
+                        continue
+                    }
+                } else {
+                    if targetName.hasSuffix("Tests") {
+                        vPrint("Ignoring \(targetName). To add cached dependencies to test files provied the target name with --target", verbose)
+                        continue
+                    }
+                }
+                for cachedDependency in cachedDependencies {
+                    let newValue = (
+                        name: cachedDependency.name,
+                        url: cachedDependency.url,
+                        version: cachedDependency.version,
+                        local: cachedDependency.localPath
+                    )
+                    if let index = dependencies.firstIndex(where: { cached.contains($0.name) }) {
+                        vPrint("Overwriting dependency \(newValue.name) from cache in \(targetName)", verbose)
+                        dependencies[index] = newValue
+                    } else {
+                        vPrint("Using cached dependency \(newValue.name) in \(targetName)", verbose)
+                        dependencies.append(newValue)
+                    }
+                }
+                targetDependencies[targetName] = dependencies
+            }
+        }
+
         let dependencies = targetDependencies
             .flatMap { $0.value }
             .reduce(into: [:]) {
@@ -42,6 +108,9 @@ struct Init: ParsableCommand {
         let manager = SpmFileManager()
         var jsonSpmFile: JsonSpmFile
         do {
+            if force {
+                throw SpmFileManager.Error.fileDoesNotExist("")
+            }
             jsonSpmFile = try manager.spmFile(in: spmfile, isVerbose: verbose)
             let existingTargetNames = jsonSpmFile.targets.map { $0.name }
             let missingTargets = Set(targetNames)
@@ -53,6 +122,21 @@ struct Init: ParsableCommand {
                         dependencies: targetDependencies[targetName]?.map { $0.name } ?? []
                     )
                 }
+            vPrint("Initializing targets:", verbose)
+            if verbose {
+                if !missingTargets.isEmpty {
+                    vPrint("\t\(missingTargets.map { $0.name }.joined(separator: ", "))", verbose)
+                } else {
+                    vPrint("\tNo new targets", verbose)
+                }
+            }
+            if !jsonSpmFile.targets.isEmpty {
+                vPrint("The following targets have not been updated:", verbose)
+                if verbose {
+                    vPrint("\t\(jsonSpmFile.targets.map { $0.name }.joined(separator: ", "))", verbose)
+                }
+                vPrint("To reinitialize all targets pass the -f flag", verbose)
+            }
             jsonSpmFile.targets = (jsonSpmFile.targets + missingTargets)
                 .sorted { $0.name < $1.name}
         } catch SpmFileManager.Error.fileDoesNotExist {
