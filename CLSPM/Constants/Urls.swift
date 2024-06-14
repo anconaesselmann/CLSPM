76ba66e3ee87f8dffb6ead5697d6fae99ff86ad9
Axel Ancona Esselmann <axel@anconaesselmann.com>
2024-06-14 00:32:41 -0700

    Adds github contributors url

--- CLSPM/Constants/Urls.swift
+++ CLSPM/Constants/Urls.swift
@@ -20,4 +20,14 @@ extension URL {
         }
         return url
     }
+
+    static func githubContributors(
+        githubUserName org: String,
+        repoName name: String
+    ) throws -> URL {
+        guard let url = URL(string: "https://api.github.com/repos/\(org)/\(name)/contributors") else {
+            throw RemoteDepenencyManager.Error.invalidUrl
+        }
+        return url
+    }
 }
