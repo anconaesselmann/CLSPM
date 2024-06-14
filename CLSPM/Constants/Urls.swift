12570c25becda47306c90b26abfa08de640b223e
Axel Ancona Esselmann <axel@anconaesselmann.com>
2024-06-13 21:59:41 -0700

    URLs constants created

---
+++ CLSPM/Constants/Urls.swift
@@ -0,0 +1,23 @@
+//  Created by Axel Ancona Esselmann on 6/13/24.
+//
+
+import Foundation
+
+extension URL {
+    static func githubRepo(githubUserName org: String, repoName name: String) throws -> URL {
+        guard let url = URL(string: "https://github.com/\(org)/\(name)") else {
+            throw RemoteDepenencyManager.Error.invalidUrl
+        }
+        return url
+    }
+
+    static func githubReleases(
+        githubUserName org: String,
+        repoName name: String
+    ) throws -> URL {
+        guard let url = URL(string: "https://api.github.com/repos/\(org)/\(name)/releases") else {
+            throw RemoteDepenencyManager.Error.invalidUrl
+        }
+        return url
+    }
+}
