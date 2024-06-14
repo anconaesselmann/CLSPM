6412e50e848958f94d32e93af777949007d71b67
Axel Ancona Esselmann <axel@anconaesselmann.com>
2024-06-13 22:03:37 -0700

    Convenience method for testing existence of a dependency in a dependencies file

--- SwiftPackageManagerCliToolsTests/Supporting/FileManagerProtocol+Extensions.swift
+++ SwiftPackageManagerCliToolsTests/Supporting/FileManagerProtocol+Extensions.swift
@@ -8,4 +8,24 @@ extension FileManagerProtocol {
         currentDirectory
             .appending(path: "spmfile")
     }
+
+    var dependenciesFileDir: URL {
+        homeDirectoryForCurrentUser
+            .appending(component: ".swiftclpm")
+            .appending(path: "dependencies")
+    }
+
+    func dependencyFile() throws -> DependenciesFile? {
+        guard let data = contents(at: dependenciesFileDir) else {
+            return nil
+        }
+        print("deconding dependencyFile in tests")
+        return try SpmFileManager.decoder.decode(DependenciesFile.self, from: data)
+    }
+}
+
+extension DependenciesFile {
+    func hasDependency(where predicate: (JsonSpmDependency) -> Bool) -> Bool {
+        dependencies.first(where: predicate) != nil
+    }
 }
